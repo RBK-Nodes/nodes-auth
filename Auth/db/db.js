d4f31157e037532848b454ca66ece689f2f56335
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-06 16:20:00 +0100

    automated message

--- Auth/db/db.js
+++ Auth/db/db.js
@@ -9,11 +9,10 @@ const pool = new Pool({
 pool.query(`CREATE TABLE users (
     id serial primary key,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-
-)`, (err, data)=>{
+    password VARCHAR(255) NOT NULL
+    );`, (err, data)=>{
     if(err) console.error(err);
-    
+
     console.log(data)
 })
 
