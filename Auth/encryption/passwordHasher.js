72b322960532f18d8931c3398a84a342c75ae70e
AdamMomen <adammuman81@gmail.com>
2020-01-06 16:49:51 +0100

    improvement of the password hasher

--- Auth/encryption/passwordHasher.js
+++ Auth/encryption/passwordHasher.js
@@ -1,10 +1,10 @@
 const bycrypt = require('bcrypt')
 //change this constat to change the salt
 var passwordHasher = (plainPassword = '', saltRounds = 0, callback) => {
-    // 
     if (!plainPassword.length) {
         return '';
     }
+
     bycrypt.genSalt(saltRounds, (err, salt) => {
         bycrypt.hash(plainPassword, salt, (err, hash) => {
             if (err) {
--- Auth/server/server.js
+++ Auth/server/server.js
@@ -16,8 +16,33 @@ var server = app.listen(port, () => {
 app.post('/signup', (req, res) => {
     //1   take username
     //2   password
-    const { username, password } = req.body
+    let { username, password } = req.body
     //3   hash the password 
+    passwordHasher(password, (err, hash) => {
+        if (err) return;
+        const hashedPass = hash
+    });
+    //4   store user and  password in the DB
+    // stord successfly? 
+    // redirect to login
+    res.redirect('/login')
+    // else ? 
+    //send 403 resonse
+    res.sendStatus(403)
+})
 
-    //4   store it in the password
+app.post('/login', (req, res) => {
+
+    //check if the user has a valid token
+    let { username, password } = req.body
+    //check if user exists in DB?
+    //get his hashedPass
+    //compare it with the hashing function
+    // matched?
+    // generate new Token
+    // store it in DB
+    //send the token back to the user
+    //redirect to main page
+    //else ?
+    //send message telling PW not correct
 })
