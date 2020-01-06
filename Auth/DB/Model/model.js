f6271de55b87ff01432704db7355d5b64f326ba0
AdamMomen <adammuman81@gmail.com>
2020-01-06 15:34:41 +0100

    postgres running

--- Auth/DB/Model/model.js
+++ Auth/DB/Model/model.js
@@ -1,8 +1,17 @@
-const Pool = require('pg').Pool
+const { Client, Pool } = require('pg')
+// user is admin and password is admin
+const connectionString = 'postgressql://postgres:admin@localhost:5433/postgres'
+// const pool = new Pool({
+//     user: 'admin',
+//     password: 'Administrator98',
+//     database: 'chatingApp',
+//     port: 5432,
+// })
 
-var pool = new Pool({
-    user: 'admin',
-    password: 'admin',
-    database: 'chatterApp'
+const client = new Client({
+    connectionString: connectionString,
 })
-module.exports = { pool }
+module.exports = {
+    client,
+    //pool
+}
--- Auth/server/server.js
+++ Auth/server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const pool = require('../db/model/model')
+const { pool, client } = require('../db/model/model')
 var app = express();
-console.log(pool)
 //changed the port to avoid conflicts
 var port = process.env.PORT || 3001
+//importing client
+client.connect(
+    (err, client, done) => {
+        if (err) throw err
+        var server = app.listen(port, () => {
+            console.log(`listenting on port ${port}`)
+        })
+    }
+)
