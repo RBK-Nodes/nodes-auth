1e3256c3ed9fb7749d86fa5140da2849922929ed
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-06 18:33:23 +0100

    automated message

--- Auth/server/server.js
+++ Auth/server/server.js
@@ -6,14 +6,13 @@ var port = process.env.PORT || 3001
 app.use(express.json());
 
 app.post('/get', (req, res)=>{
-    res.status(200).send("get route");
-    // User.find(req.body.username)
-    // .then(data=>{
-    //     res.status(200).send("got data", data)
-    // })
-    // .catch(err=>{
-    //     res.status(200).send("got error ", err)
-    // })
+    User.find(req.body.username)
+    .then(data=>{
+        res.status(200).send("found user", data)
+    })
+    .catch(err=>{
+        res.status(404).send("user not found ", err)
+    })
 })
 
 app.post('/create', (req, res)=>{
